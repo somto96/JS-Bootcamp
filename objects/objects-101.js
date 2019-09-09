4cf28e5c0213fbb31e9ceda2f50ce516340d9476
somto96 <motivatedsomto@gmail.com>
2019-09-09 23:28:31 +0100

    JS objects

---
+++ objects/objects-101.js
@@ -0,0 +1,11 @@
+let person = {
+    name: 'Somto',
+    age: 23,
+    location: 'Lagos'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age = 24
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
